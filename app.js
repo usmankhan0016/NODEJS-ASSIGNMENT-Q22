3543862c40b46a4acec6708948f2de0a656a1690
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-07 14:00:59 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+//  If you haven’t received an array index error in one of your programs yet, try to make one happen.
+let Days = ["Monday", "Thursday", "Friday"];
+console.log(Days[3]);
+// Make sure you correct the error before closing the program.
+console.log(Days);
---
+++ app.ts
@@ -0,0 +1,7 @@
+//  If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+let Days: string[]=["Monday","Thursday","Friday"];
+console.log(Days[3]);
+
+// Make sure you correct the error before closing the program.
+console.log(Days);
